d2d4cb0163bfd94ed894acf613e31d9c901eedf4
Mahdi <mahdimeyghani02@gmail.com>
2024-08-11 12:08:23 +0330

    Specifying the columns to display in admin interface

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Form
 
-admin.site.register(Form)
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "date", "occupation")
+
+
+admin.site.register(Form, FormAdmin)
 
