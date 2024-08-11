d5eb0b549c3c062d3bc619d96d1bcd7008cc60f0
Mahdi <mahdimeyghani02@gmail.com>
2024-08-11 16:58:16 +0330

    Ordering the model admin interface by 'date'

--- job_application/admin.py
+++ job_application/admin.py
@@ -6,6 +6,7 @@ class FormAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name", "email", "date", "occupation")
     search_fields = ("first_name", "last_name")
     list_filter = ("date", "occupation")
+    ordering = ("date", )
 
 
 admin.site.register(Form, FormAdmin)
