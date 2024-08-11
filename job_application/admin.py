28f810eead857aea4e1f05b0c53aff09c5ec16ab
Mahdi <mahdimeyghani02@gmail.com>
2024-08-11 12:10:54 +0330

    Specifying the search fields by first_name and last_name in admin model interface

--- job_application/admin.py
+++ job_application/admin.py
@@ -4,6 +4,7 @@ from .models import Form
 
 class FormAdmin(admin.ModelAdmin):
     list_display = ("first_name", "last_name", "email", "date", "occupation")
+    search_fields = ("first_name", "last_name")
 
 
 admin.site.register(Form, FormAdmin)
